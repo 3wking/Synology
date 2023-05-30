c9ecc1e5cb1a880db5f3eae06e9a67f4ebef6b24
3wking <1094890624@qq.com>
2023-05-30 22:03:32 +0800

    更新 Power.sh

--- Shell/Power.sh
+++ Shell/Power.sh
@@ -6,7 +6,10 @@ for((i=1;i<=60;i++));do
 		time=$(date "+%Y-%m-%d %H:%M:%S")
 		echo $time
 		curl -H "Content-Type:application/json" -X POST -d '{"message_type":"private","user_id":"1094890624","message":"群晖开机通知\n开机时间:\n'"$time"'"}' "http://localhost:5700/send_msg"
+		text="成功"
 		break
 	fi
 	sleep 10
+	text="失败"
 done
+synodsmnotify -t dsm @administrators "开机通知" "发送开机通知：$text." >/dev/null
