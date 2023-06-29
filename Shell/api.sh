f5f45957ccae2776ab10ad98caa4ab82a24f1465
3wking <43069914+3wking@users.noreply.github.com>
2023-06-29 17:44:02 +0800

    创建 api.sh

---
+++ Shell/api.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#首次启动
+if ! ps aux | grep -w mysqld | grep -v grep >/dev/null 2>&1
+Synoapi
+synodsmnotify -t dsm @administrators "API通知" "API启动" >/dev/null
+else
+synodsmnotify -t dsm @administrators "API通知" "API已启动" >/dev/null
+fi
+#进程守护
+while :
+do
+if ! ps aux | grep -w mysqld | grep -v grep >/dev/null 2>&1
+then
+Synoapi
+synodsmnotify -t dsm @administrators "API通知" "API启动" >/dev/null
+fi
+sleep 600
+done
