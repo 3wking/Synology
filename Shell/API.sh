1ed1151ac738f38b7a15aed915b5417be71c4580
3wking <43069914+3wking@users.noreply.github.com>
2023-06-29 16:48:39 +0800

    创建 API.sh

---
+++ Shell/API.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for((i=1;i<=60;i++));do
+	echo "运行次数"$i
+	./Synoapi
+	status_code=$(curl -s -o /dev/null -w "%{http_code}" -X POST "http://127.0.0.1:1001")
+	if [[ $status_code == 200 ]]; then
+		synodsmnotify -t dsm @administrators "API通知" "API启动成功" >/dev/null
+		break
+	fi
+	sleep 10
+done
