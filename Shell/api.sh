8449920bbce74f39088044784f7a65d8e806c0ab
3wking <43069914+3wking@users.noreply.github.com>
2023-06-30 14:23:01 +0800

    更新 api.sh

--- Shell/api.sh
+++ Shell/api.sh
@@ -2,7 +2,7 @@
 #首次启动
 if ! ps aux | grep -w Synoapi | grep -v grep >/dev/null 2>&1
 then
-bash Synoapi
+sudo /usr/syno/bin/Synoapi
 echo -e "\e[1;32m启动api\e[0m"
 sleep 5s
 fi
@@ -11,7 +11,7 @@ while :
 do
 if ! ps aux | grep -w Synoapi | grep -v grep >/dev/null 2>&1
 then
-bash Synoapi
+sudo /usr/syno/bin/Synoapi
 echo -e "\e[1;32m守护启动api进程\e[0m"
 fi
 sleep 600
